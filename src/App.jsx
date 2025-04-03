1d0eb7ce9bf600557204e05e54e9b3d5071c6c74
Binary Digitz <borisndoh@gmail.com>
2025-04-03 21:28:50 +0100

    Add search bar

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,13 @@ import { Home } from "./components/exportComp";
 function App() {
   // const API_URL =  'http://www.omdpapi.com?apikey=229fa716'
   const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=229fa716";
-
+ const movie1 = {
+  "Title": "Superman, Spiderman or Batman",
+  "Year": "2011",
+  "imdbID": "tt2084949",
+  "Type": "movie",
+  "Poster": "https://m.media-amazon.com/images/M/MV5BMjQ4MzcxNDU3N15BMl5BanBnXkFtZTgwOTE1MzMxNzE@._V1_SX300.jpg"
+}
   async function fetchMovies(title) {
     try {
       const response = await fetch(`${API_URL}&S=${title}`);
@@ -20,7 +26,20 @@ function App() {
     fetchMovies("spiderman");
   }, []);
   return (
-    <div>
+    <div className="app">
+      <h1>Movieland</h1>
+      <div className="search">
+        <input type="text" 
+        value={"Superman"} 
+        onChange={(event) =>  {}}
+        placeholder="Search movies" />
+        <img src=''
+        onClick={() =>{}}
+         alt="search" />
+      </div>
+      <div className="container">
+
+      </div>
       <header>header</header>
       <main>
         <Routes>
